022a67e67566cef7a0184336394b335f8c94d2e4
Bojing <binfan@visionatics.com.tw>
2023-02-18 00:37:58 +0800

    init

---
+++ CustomerSystem/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace CustomerSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_MobileNumber = new System.Windows.Forms.TextBox();
+            this.dgv_DBList = new System.Windows.Forms.DataGridView();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.txt_SearchPattern = new System.Windows.Forms.TextBox();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_DatabastButton = new System.Windows.Forms.Button();
+            this.txt_DBPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_DBList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Enabled = false;
+            this.btn_Add.Location = new System.Drawing.Point(102, 154);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 0;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(102, 15);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(100, 22);
+            this.txt_Name.TabIndex = 1;
+            // 
+            // txt_MobileNumber
+            // 
+            this.txt_MobileNumber.Location = new System.Drawing.Point(102, 46);
+            this.txt_MobileNumber.Name = "txt_MobileNumber";
+            this.txt_MobileNumber.Size = new System.Drawing.Size(100, 22);
+            this.txt_MobileNumber.TabIndex = 2;
+            // 
+            // dgv_DBList
+            // 
+            this.dgv_DBList.AllowUserToAddRows = false;
+            this.dgv_DBList.AllowUserToDeleteRows = false;
+            this.dgv_DBList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_DBList.Location = new System.Drawing.Point(385, 43);
+            this.dgv_DBList.Name = "dgv_DBList";
+            this.dgv_DBList.ReadOnly = true;
+            this.dgv_DBList.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.dgv_DBList.RowTemplate.Height = 24;
+            this.dgv_DBList.Size = new System.Drawing.Size(455, 185);
+            this.dgv_DBList.TabIndex = 4;
+            this.dgv_DBList.DoubleClick += new System.EventHandler(this.dgvDBList_DoubleClick);
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Enabled = false;
+            this.btn_Search.Location = new System.Drawing.Point(591, 236);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 24);
+            this.btn_Search.TabIndex = 5;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // txt_SearchPattern
+            // 
+            this.txt_SearchPattern.Location = new System.Drawing.Point(485, 236);
+            this.txt_SearchPattern.Name = "txt_SearchPattern";
+            this.txt_SearchPattern.Size = new System.Drawing.Size(100, 22);
+            this.txt_SearchPattern.TabIndex = 6;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Enabled = false;
+            this.btn_Delete.Location = new System.Drawing.Point(264, 154);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 7;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Enabled = false;
+            this.btn_Update.Location = new System.Drawing.Point(183, 154);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 8;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Address.Location = new System.Drawing.Point(102, 75);
+            this.txt_Address.Multiline = true;
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(277, 73);
+            this.txt_Address.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label8.Location = new System.Drawing.Point(12, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 28);
+            this.label8.TabIndex = 344;
+            this.label8.Text = "姓名";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 28);
+            this.label1.TabIndex = 345;
+            this.label1.Text = "電話";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label5.Location = new System.Drawing.Point(12, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 28);
+            this.label5.TabIndex = 346;
+            this.label5.Text = "地址";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_DatabastButton
+            // 
+            this.btn_DatabastButton.Location = new System.Drawing.Point(8, 295);
+            this.btn_DatabastButton.Name = "btn_DatabastButton";
+            this.btn_DatabastButton.Size = new System.Drawing.Size(123, 23);
+            this.btn_DatabastButton.TabIndex = 347;
+            this.btn_DatabastButton.Text = "先選擇資料庫";
+            this.btn_DatabastButton.UseVisualStyleBackColor = true;
+            this.btn_DatabastButton.Click += new System.EventHandler(this.btn_DatabastButton_Click);
+            // 
+            // txt_DBPath
+            // 
+            this.txt_DBPath.Location = new System.Drawing.Point(135, 322);
+            this.txt_DBPath.Name = "txt_DBPath";
+            this.txt_DBPath.ReadOnly = true;
+            this.txt_DBPath.Size = new System.Drawing.Size(708, 22);
+            this.txt_DBPath.TabIndex = 348;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(8, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 22);
+            this.label2.TabIndex = 349;
+            this.label2.Text = "SQLConnect String";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(383, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 22);
+            this.label3.TabIndex = 350;
+            this.label3.Text = "搜尋名稱";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label4.Location = new System.Drawing.Point(383, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(457, 22);
+            this.label4.TabIndex = 351;
+            this.label4.Text = "搜尋結果";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 354);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_DBPath);
+            this.Controls.Add(this.btn_DatabastButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.txt_SearchPattern);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.dgv_DBList);
+            this.Controls.Add(this.txt_MobileNumber);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.btn_Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_DBList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_MobileNumber;
+        private System.Windows.Forms.DataGridView dgv_DBList;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox txt_SearchPattern;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_DatabastButton;
+        private System.Windows.Forms.TextBox txt_DBPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CustomerSystem/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CustomerSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int CurrentSelectedCutsomerID = -1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(txt_DBPath.Text);
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                SqlCommand sqlCmd = new SqlCommand("CustomerAddOrEdit", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                sqlCmd.Parameters.AddWithValue("@Id", 0);
+                sqlCmd.Parameters.AddWithValue("@Name", txt_Name.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@MobileNumber", txt_MobileNumber.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Address", txt_Address.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Saved successfully");
+                RefreshDBList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(txt_DBPath.Text);
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SearchCustomer", sqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("@CustomerName", txt_SearchPattern.Text.Trim());
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+                dgv_DBList.DataSource = dtbl;
+                dgv_DBList.Columns[0].Visible = false;
+                sqlCon.Close();
+                Reset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+
+        private void dgvDBList_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgv_DBList.CurrentRow.Index != -1)
+            {
+                CurrentSelectedCutsomerID = Convert.ToInt32(dgv_DBList.CurrentRow.Cells[0].Value.ToString());
+                txt_Name.Text = dgv_DBList.CurrentRow.Cells[1].Value.ToString();
+                txt_MobileNumber.Text = dgv_DBList.CurrentRow.Cells[2].Value.ToString();
+                txt_Address.Text = dgv_DBList.CurrentRow.Cells[3].Value.ToString();
+                btn_Update.Enabled = btn_Delete.Enabled = true;
+
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("是否刪除該筆資料",
+                                     "是否刪除",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult != DialogResult.Yes)
+            {
+                return;
+            }
+ 
+            SqlConnection sqlCon = new SqlConnection(txt_DBPath.Text);
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("CustomerDeletion", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("Id", CurrentSelectedCutsomerID);
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted successfully");
+
+                RefreshDBList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+
+        private void Reset()
+        {
+
+            txt_Name.Text = txt_MobileNumber.Text = txt_Address.Text = "";
+            btn_Update.Enabled = false;
+            CurrentSelectedCutsomerID = -1;
+            btn_Delete.Enabled = false;
+        }
+
+        private void RefreshDBList()
+        {
+            try
+            {
+                Reset();
+                SqlConnection sqlCon = new SqlConnection(txt_DBPath.Text);
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SearchCustomer", sqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("@CustomerName", "");
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+                dgv_DBList.DataSource = dtbl;
+                dgv_DBList.Columns[0].Visible = false;
+                sqlCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(txt_DBPath.Text);
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                SqlCommand sqlCmd = new SqlCommand("CustomerAddOrEdit", sqlCon);
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@mode", "Edit");
+                sqlCmd.Parameters.AddWithValue("@Id", CurrentSelectedCutsomerID);
+                sqlCmd.Parameters.AddWithValue("@Name", txt_Name.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@MobileNumber", txt_MobileNumber.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Address", txt_Address.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Updated successfully");
+                RefreshDBList();
+                Reset();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+
+        private void btn_DatabastButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "mdf|*.mdf";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txt_DBPath.Text = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + openFileDialog.FileName + ";Integrated Security=True;Connect Timeout=30";
+                btn_Add.Enabled = true;
+                btn_Search.Enabled = true;
+            }
+        }
+    }
+}
